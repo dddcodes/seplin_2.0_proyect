5e74c1a65f149ebfb65e52dae4dff585a00f6fee
dddcodes <karatoyt422@gmail.com>
2025-03-24 21:29:55 -0700

    PRACTICE: Opciones se revuelven de manera random junto a la respuesta correcta, y para testear la respuesta se resalta en el CSS

--- views/practice.js
+++ views/practice.js
@@ -3,40 +3,49 @@ import { CONFIG } from "../config.js";
 import { LSM } from "../localStorageManager.js";
 
 const actualView = CONFIG.routes.practice;
-
 export default () => {
   u.setPageTitle(actualView.title);
 
   const localQuizzes = LSM.getLocalQuizzes();
+  const localQuizzesLength = Object.keys(localQuizzes).length;
+  const randomNumber = u.random(1, localQuizzesLength);
+  const actualQuiz = localQuizzes[randomNumber];
 
-  let localQuizzesLength = Object.keys(localQuizzes).length;
-  let randomNumber = u.random(1, localQuizzesLength);
+  // Función para mezclar arrays (Fisher-Yates shuffle)
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
 
-  let actualQuiz = localQuizzes[randomNumber];
+  // Combinar opciones incorrectas con la respuesta correcta y mezclar
+  const allOptions = shuffleArray([...actualQuiz.options, actualQuiz.answer]);
+
   console.log(actualQuiz);
 
   return `
-        <div>
-            <div class="quizBox">
-
-                <p class="details">Deportes 401</p>
-
-                <p class="question">${actualQuiz.question}</p>
-                <div class="optionsBox">
-                ${actualQuiz.options
-                  .map(
-                    (option, index) => `
-                    <button class="option" value="${index}" onclick="">
-                        ${index + 1} - ${option}
-                    </button>`
-                  )
-                  .join("")}
-                    <button class="option" value="VALUE" onclick="">
-                        4 - ${actualQuiz.answer}
-                    </button>
-                </div>
-
-            </div>
+    <div>
+      <div class="quizBox">
+        <p class="details">Deportes 401</p>
+        <p class="question">${actualQuiz.question}</p>
+        <div class="optionsBox">
+          ${allOptions
+            .map(
+              (option, index) => `
+            <button class="option" 
+                    value="${
+                      option === actualQuiz.answer ? "correct" : "incorrect"
+                    }" 
+                    onclick="">
+              ${index + 1} - ${option}
+            </button>`
+            )
+            .join("")}
         </div>
-    `;
+      </div>
+    </div>
+  `;
 };
