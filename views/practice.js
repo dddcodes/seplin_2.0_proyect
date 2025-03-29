8d5c460f1586fd0896b0e3149f09578e48bf7188
dddcodes <karatoyt422@gmail.com>
2025-03-28 20:07:00 -0700

    CSS: detalles diminutos en las fuentes y margenes

--- views/practice.js
+++ views/practice.js
@@ -30,41 +30,45 @@ export default () => {
 
   const backButton = document.querySelector("#backButton");
 
+
   return `
     <div id="practiceLayout">
       <button id="backButton" onClick='
 
-      window.history.back();
+        window.history.back();
 
-      const sidebar = document.querySelector("#sidebar");
-      const navbar = document.querySelector("#navigationBar");
-      sidebar.style.display = "flex";
-      navbar.style.display = "flex";
+        const sidebar = document.querySelector("#sidebar");
+        const navbar = document.querySelector("#navigationBar");
+        sidebar.style.display = "flex";
+        navbar.style.display = "flex";
 
-      window.scrollTo(0, 0);
+        window.scrollTo(0, 0);
 
-      '>X</button>
-        <div class="quizBox">
+      '>X
+      </button>
+    
 
-        
-          <p class="details">Deportes 401</p>
-          <p class="question">${actualQuiz.question}</p>
+      <div class="quizBox">
 
-          <div class="optionsBox">
-            ${allQuizOptions
-              .map(
-                (option, index) => `
-              <button class="option" value="${
-                option === actualQuiz.answer ? "correct" : "incorrect"
-              }" 
-                      onclick="">
-                ${index + 1} - ${option}
-              </button>`
-              )
-              .join("")}
-          </div>
+      
+        <p class="details">Deportes 401</p>
+        <p class="question">${actualQuiz.question}</p>
 
+        <div class="optionsBox">
+          ${allQuizOptions
+            .map(
+              (option, index) => `
+            <button class="option" value="${
+              option === actualQuiz.answer ? "correct" : "incorrect"
+            }" 
+                    onclick="">
+              ${index + 1} - ${option}
+            </button>`
+            )
+            .join("")}
         </div>
+
+      </div>
         
       <div id="practiceBar">
         <button id="explainButton">EXPLICAR</button>
