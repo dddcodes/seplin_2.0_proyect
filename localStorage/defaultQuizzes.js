2a7ce5cb0ba7c88d1ecd21301fa026b6eef85389
dddcodes <karatoyt422@gmail.com>
2025-03-29 14:01:00 -0700

    Actualización

--- localStorage/defaultQuizzes.js
+++ localStorage/defaultQuizzes.js
@@ -7,18 +7,21 @@ export default () => {
      answer: "París",
      explanation: "París ha sido la capital de Francia desde el año 508.",
      options: ["Lyon", "Marsella", "Toulouse"],
+     hitScore: [false,false,false,false,false]
    },
    [uuidv4()]: {
      question: "¿En qué año llegó el hombre a la Luna?",
      answer: "1969",
      explanation: "El Apolo 11 aterrizó en la Luna el 20 de julio de 1969.",
      options: ["1955", "1975", "1989"],
+     hitScore: [false,false,false,false,false]
    },
    [uuidv4()]: {
      question: "¿Qué elemento químico tiene el símbolo 'O'?",
      answer: "Oxígeno",
      explanation: "El oxígeno es esencial para la respiración y la combustión.",
      options: ["Oro", "Osmio", "Oganesón"],
+     hitScore: [false,false,false,false,false]
    },
  });
  console.log(LSM.getLocalQuizzes());
