9d6589103ca8ea59a2d13551645bd3cf0310a0f3
dddcodes <karatoyt422@gmail.com>
2025-04-17 13:34:22 -0700

    Actualización

--- localStorage/defaultQuizzes.js
+++ localStorage/defaultQuizzes.js
@@ -1,7 +1,7 @@
 import { LSM } from "../localStorage/localStorageManager.js";
 
 export default () => {
-  LSM.RESET(); //RESETEA EL LOCALSTORAGE
+  //LSM.RESET(); //RESETEA EL LOCALSTORAGE
 
   if (!localStorage.getItem("localQuizzes")) {
     LSM.createItem("localQuizzes");
@@ -30,31 +30,3 @@ export default () => {
     console.log(LSM.getLocalQuizzes());
   }
 };
-/*
-FORMATO DE QUIZZES:
-
-[
-  {
-    "question": "Here goes the question",
-    "answer": "Here goes the CORRECT answer",
-    "feedback": "Here goes an explanation to find out what is the answer and why (very short)",
-    "options": ["incorrect option 1", "incorrect option 2", "incorrect option 3"]
-  },
-  {
-    "question": "Here goes the question",
-    "answer": "Here goes the CORRECT answer",
-    "feedback": "Here goes an explanation to find out what is the answer and why (very short)",
-    "options": ["incorrect option 1", "incorrect option 2", "incorrect option 3"]
-  },
-]
-
-[
-  {
-    "question": "¿Cuál es la capital de Francia?",
-    "answer": "París",
-    "feedback": "Correcto!",
-    "options": ["Londres", "Berlín", "Madrid", "París"]
-  }
-]
-
-*/
